b2e9ac58746b758506f7925ed1a4da8b7c3d50a3
Mariabenaouf <mariabenaoufnm3@gmail.com>
2024-11-28 11:37:35 +0100

    adding file

--- fonctions.py
+++ fonctions.py
@@ -1,4 +1,4 @@
-
+ 
 def addition(x, y):
     """Retourne la somme de x et y"""
     return 
@@ -6,7 +6,7 @@ def addition(x, y):
 
 def soustraction(x, y):
     """Retourne la différence de x et y"""
-    return 
+    return x-y
 
 
 def noms_binome():
